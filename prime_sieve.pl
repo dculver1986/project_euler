487508ecc19c1f078ecca778d2648bd497da156f
Daniel Culver <dculver@hostgator.com>
2015-06-02 08:50:33 -0500

    Add prime sieve to repo

---
+++ prime_sieve.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+
+sub sieve {
+  my $n = shift;
+  my @composite;
+  for my $i ( 2..int( sqrt($n) ) ) {
+    if ( !$composite[$i] ) {
+      for ( my $j = $i * $i; $j <= $n; $j += $i ) {
+        $composite[$j] = 1;
+      }
+    }
+  }
+  my @primes;
+  for my $i ( 2 .. $n ) {
+    $composite[$i] || push @primes, $i;
+  }
+   return @primes;
+}
